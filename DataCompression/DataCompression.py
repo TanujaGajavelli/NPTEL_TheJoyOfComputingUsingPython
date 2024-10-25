ffcb71f2293d536b6edd78e4752fc99db370aca4
TanujaGajavelli <tanujasai20@gmail.com>
2024-10-25 13:52:46 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Anagrams.py

---
+++ DataCompression/DataCompression.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image
+im=Image.open('image.png').convert('L')
+pixelMap=im.load()
+# I=np.asanyarray(Image.open('image.png')) #as any array
+img=Image.new(im.mode,im.size)
+pixelNew=img.load()
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        pixelValue=pixelMap[i,j]
+        if(pixelValue>=0 and pixelValue<=31):
+            pixelNew[i,j]=0
+        elif (pixelMap[i, j] >= 32 and pixelMap[i, j] <= 63):
+            pixelNew[i, j] = 1
+        elif (pixelMap[i, j] >= 64 and pixelMap[i, j] <= 95):
+            pixelNew[i, j] = 2
+        elif (pixelMap[i, j] >= 96 and pixelMap[i, j] <= 127):
+            pixelNew[i, j] = 3
+        elif (pixelMap[i, j] >= 128 and pixelMap[i, j] <= 159):
+            pixelNew[i, j] = 4
+        elif (pixelMap[i, j] >= 160 and pixelMap[i, j] <= 191):
+            pixelNew[i, j] = 5
+        elif (pixelMap[i, j] >= 192 and pixelMap[i, j] <= 223):
+            pixelNew[i, j] = 6
+        elif (pixelMap[i, j] >= 224 and pixelMap[i, j] <= 255):
+            pixelNew[i, j] = 7
+img.save('imageCompressed.png')
+J=np.asanyarray(Image.open('imageCompressed.png'))
